6ab6c8da3ba27209a0d45f85f7b4088ac708b5a9
methas-ru <methas_ru@cmu.ac.th>
2025-07-30 22:24:39 +0700

    test diference between normal user and admin

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("\n");
+
+        // test admin
+
         Admin john = new Admin("John",2000,7,30);
 
-        //test display info
-
-        System.out.println("\n");
-
         john.displayInfo(true);
-        System.out.println("\n");
-
         System.out.println(john.isBirthday());
         john.displayHappyBirthday();
+        System.out.println("\n");
+
+        // test normal user
+
+        User nicolas = new User("Nicolas",1990,7,30);
+
+        nicolas.displayInfo();
+        System.out.println(nicolas.isBirthday());
+        nicolas.displayHappyBirthday();
+        System.out.println("\n");
 
     }
 }
